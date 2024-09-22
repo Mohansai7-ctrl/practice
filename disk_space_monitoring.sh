a8e1c633659ec3e7b27915b65764046a6f990aac
Mohansai <bandarumohansai@gmail.com>
2024-09-22 18:59:57 +0530

    check.sh practice

--- disk_space_monitoring.sh
+++ disk_space_monitoring.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 userid=$(id -u)
 
-# USER(){
-#     if [ ${userid} -ne 0 ]
-#         echo "As you didnt have root access, pls run scripts with root access to proceed further."
-#         exit 1
-#     else
-#         echo "Hurray! You are having root access, hence proceeding further."
-#     fi
+USER(){
+    if [ ${userid} -ne 0 ]
+        echo "As you didnt have root access, pls run scripts with root access to proceed further."
+        exit 1
+    else
+        echo "Hurray! You are having root access, hence proceeding further."
+    fi
 
-# }
+}
 
-# VALIDATE(){
-#     if [ $1 -ne 0 ]
-#     then
-#         echo "are FAILED"
-#     else
-#         echo "are SUCCESS"
-#     fi
-# }
-
-
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "are FAILED"
+    else
+        echo "are SUCCESS"
+    fi
+}
 
 
-#USER
+
+
+USER
 
 DISK_FILES=$(df -hT | grep xfs)
 Threshold=50
@@ -36,9 +36,9 @@ if [ $DISK_USED -gt $Threshold ]
 then
     echo "Below files are execeed the disk usage more than threshold percentage, $DISK_USED"
     echo "Those filesystems mounted on $MOUNTED_ON"
-    #VALIDATE $? "Extraction of filesystems"
+    VALIDATE $? "Extraction of filesystems"
 else
-    echo "Dont have any files which exceed the $Threshold
+    echo "Dont have any files which exceed the $Threshold"
 fi
 
 
